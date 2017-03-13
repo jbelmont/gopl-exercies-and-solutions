12c9af7f9841144fff20362f1c6696051e79e483
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-13 18:52:25 -0400

    Add time and benchmark tests.

--- ch1/echo-ex1_test.go
+++ ch1/echo-ex1_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -19,4 +20,32 @@ func TestEcho2(t *testing.T) {
 	idx, actual := echo2(args)
 	assert.Equal(t, len(idx), 4)
 	assert.NotNil(t, actual)
+}
+
+/* Experiment to measure the difference in running time between our potentially inefficient versions and the one that uses strings.Join.
+ * Section 1.6 illustrates part of the time package
+ * Section 11.4 shows how to write benchmark tests for systematic performance evaluation.)
+ */
+func TestTimeForEcho1(t *testing.T) {
+	start := time.Now()
+	echo([]string{"arg1", "arg2", "arg3", "arg4"})
+	assert.NotNil(t, time.Since(start).Seconds())
+}
+
+func TestTimeForEcho2(t *testing.T) {
+	start := time.Now()
+	echo2([]string{"arg1", "arg2", "arg3", "arg4"})
+	assert.NotNil(t, time.Since(start).Seconds())
+}
+
+func BenchmarkEcho1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		echo([]string{"arg1", "arg2", "arg3", "arg4"})
+	}
+}
+
+func BenchmarkEcho2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		echo2([]string{"arg1", "arg2", "arg3", "arg4"})
+	}
 }
