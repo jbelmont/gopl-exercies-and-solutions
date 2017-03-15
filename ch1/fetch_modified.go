2f34df7904933a02dfde8922eae1acc66ca7f92a
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-14 20:14:13 -0400

    Add modified fetch go file.

---
+++ ch1/fetch_modified.go
@@ -0,0 +1,40 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+/*
+ * Exercise 1.7: The function call io.Copy(dst, src) reads from src and writes to dst. 
+ * Use it instead of ioutil.ReadAll to copy the response body to os.Stdout without requiring a buffer large enough to hold the entire stream. 
+ * Be sure to check the error result of io.Copy.
+ */
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%d", b)
+	}
+}
